7f78ea52b08313d4b3be858d78f25323afa1d36b
Dmitry Lipnitskij <dmitrydust2704@gmail.com>
2023-03-14 19:48:59 +0300

    GOSTCrypt class init

---
+++ gost.py
@@ -0,0 +1,28 @@
+import numpy
+
+
+class GOSTCrypt:
+    __slots__ = {
+        '__key',
+        '__key_table',
+    }
+
+    def __init__(self, key, key_table):
+        self.key = key
+        self.key_table = key_table
+
+    @property
+    def key(self):
+        return self.__key
+
+    @key.setter
+    def key(self, key):
+        self.__key = key
+
+    @property
+    def key_table(self):
+        return self.__key_table
+
+    @key_table.setter
+    def key_table(self, key_table):
+        self.__key_table = key_table
