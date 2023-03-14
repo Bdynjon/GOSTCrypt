5ab4db30e57e46652a27502064b6b26d0abe63df
Dmitry Lipnitskij <dmitrydust2704@gmail.com>
2023-03-14 20:32:04 +0300

    gost: key validation

--- gost.py
+++ gost.py
@@ -1,4 +1,5 @@
-import numpy
+import numpy as np
+import operations as op
 
 
 class GOSTCrypt:
@@ -7,7 +8,7 @@ class GOSTCrypt:
         '__key_table',
     }
 
-    def __init__(self, key, key_table):
+    def __init__(self, key=None, key_table=None):
         self.key = key
         self.key_table = key_table
 
@@ -17,8 +18,12 @@ class GOSTCrypt:
 
     @key.setter
     def key(self, key: str):
-
-        self.__key = key
+        if type(key) == str:
+            key = op.text_to_bits(key)
+            if len(key) != 64: raise ValueError('Ключ должен быть строкой в 64 бита')
+            self.__key = key
+        else:
+            self.__key = None
 
     @property
     def key_table(self):
@@ -27,3 +32,13 @@ class GOSTCrypt:
     @key_table.setter
     def key_table(self, key_table):
         self.__key_table = key_table
+
+
+def main():
+    gost = GOSTCrypt()
+    key = 'qwertyui'
+    gost.key = key
+
+
+if __name__ == "__main__":
+    main()
--- operations.py
+++ operations.py
@@ -12,32 +12,12 @@ def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
 
 
-def encode_string(string: str):
-    array = map(text_to_bits, string)
-    array = list(reduce(lambda res, el: res+el, array))
-    size = len(array)
-    size_code = list(bin(size)[2:].zfill(15))
-
-    return size_code + array
-
-
-def decode_string(array):
-    size_text = "".join(array[:15])
-    size = int("0b" + size_text[::-1], 2)
-
-    bits_string = "".join(array[15:])
-    text = ""
-    for i in range(8, size+8, 8):
-        try:
-            text += text_from_bits(bits_string[i-8:i])
-        except:
-            continue
-
-    return text
-
-
 def main():
-    pass
+    test_message = "Текст message"
+
+    a = text_to_bits('test_message')
+    print('1', a, "  len = ", len(a))
+    print('2', text_from_bits(a))
 
 
 if __name__ == "__main__":
