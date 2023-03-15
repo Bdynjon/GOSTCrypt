eea476254041e06e014db54750195f74a946564f
Dmitry Lipnitskij <dmitrydust2704@gmail.com>
2023-03-15 16:53:05 +0300

    gost: simple_replace finished

--- gost.py
+++ gost.py
@@ -1,7 +1,6 @@
 import numpy as np
 import operations as op
 import itertools
-import functools
 
 
 class GOSTCrypt:
@@ -14,11 +13,60 @@ class GOSTCrypt:
         self.key = key
         self.key_table = key_table
 
-    def encrypt(self, text: str):
-        pass
+    def simple_replace_encr(self, text):
+        text = op.text_to_bits(text)
+        if len(text) % 64 != 0: raise ValueError('Размер текста должен быть кратен 64 бит')
+        text = [int(text[part_num * 64: part_num * 64 + 64], 2) for part_num in range(len(text)//64)]
 
-    def __main_operation(self):
-        pass
+        result = ''
+        for block in text:
+            result += (bin(self.__encrypt_block(block))[2:]).zfill(64)
+
+        return op.btext_from_bits(result)
+
+    def simple_replace_decr(self, text):
+        text = op.btext_to_bits(text)
+        if len(text) % 64 != 0: raise ValueError('Размер шифр-текста должен быть кратен 64 бит')
+        text = [int(text[part_num * 64: part_num * 64 + 64], 2) for part_num in range(len(text) // 64)]
+
+        result = ''
+        for block in text:
+            result += (bin(self.__decrypt_block(block))[2:]).zfill(64)
+
+        return op.text_from_bits(result)
+
+    def __encrypt_block(self, block):
+        left, right = block >> 32, block & 0xFFFFFFFF
+
+        for i in range(24):
+            left, right = self.__main_operation(left, right, self.key[i % 8])
+
+        for i in range(7, -1, -1):
+            left, right = self.__main_operation(left, right, self.key[i])
+
+        return (right << 32) | left
+
+    def __decrypt_block(self, block):
+        left, right = block >> 32, block & 0xFFFFFFFF
+
+        for i in range(8):
+            left, right = self.__main_operation(left, right, self.key[i])
+
+        for i in range(23, -1, -1):
+            left, right = self.__main_operation(left, right, self.key[i % 8])
+
+        return (right << 32) | left
+
+    def __main_operation(self, left, right, key):
+        temp = right ^ key
+        res = 0
+
+        for i in range(8):
+            res |= self.key_table[i][(temp >> i*4) & 0b1111] << i*4
+        res = ((res >> (32-11)) | (res << 32)) & 0xFFFFFFFF
+        left ^= res
+
+        return right, left
 
     @property
     def key(self):
@@ -28,8 +76,8 @@ class GOSTCrypt:
     def key(self, key: str):
         if type(key) == str:
             key = op.text_to_bits(key)
-            if len(key) != 32: raise ValueError('Ключ должен быть строкой в 32 бита')
-            self.__key = key
+            if len(key) != 256: raise ValueError('Ключ должен быть строкой в 32 бита')
+            self.__key = [int(key[part_num*32: part_num*32 + 32], 2) for part_num in range(8)]
         else:
             self.__key = None
 
@@ -52,13 +100,16 @@ class GOSTCrypt:
 
 def main():
     gost = GOSTCrypt()
-    key = 'qwer'
+    key = 'qwer17lkybz5f9up3m4h08nkudjhhtgf'
     key_table = [np.random.permutation(row) for row in itertools.repeat(range(16), 8)]
-    print(key_table)
     gost.key = key
     gost.key_table = key_table
-    print(gost.key_table)
 
+    text = '1234567891230458'
+
+    cipher_text = gost.simple_replace_encr(text)
+    print(cipher_text)
+    print(gost.simple_replace_decr(cipher_text))
 
 
 if __name__ == "__main__":
--- operations.py
+++ operations.py
@@ -7,9 +7,19 @@ def text_to_bits(text, encoding="utf-8", errors="surrogatepass"):
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
 
+def btext_to_bits(text, encoding="utf-8", errors="surrogatepass"):
+    bits = bin(int.from_bytes(text, 'big'))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+
 def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
     n = int(bits, 2)
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
+
+
+def btext_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big') or '\0'
 
 
 def has_unique_rows(array):
@@ -21,10 +31,13 @@ def has_unique_rows(array):
 
 
 def main():
-    test_message = "Текст message"
+    test_message = '1234567891230458'
 
     a = text_to_bits(test_message)
     print('1', a, "  len = ", len(a))
+    print(int(a, 2))
+    print(int('11010100110110001101010001010100110001001100100011000100100001011000000110100001101110000010100111001001100010011000001111110', 2))
+    print(text_from_bits('11010100110110001101010001010100110001001100100011000100100001011000000110100001101110000010100111001001100010011000001111110', encoding='ascii'))
     print('2', text_from_bits(a))
 
 
